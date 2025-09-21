6f711b111f20ebdbab0d30de555128f42cdb63fe
taciov <140907042+taciov@users.noreply.github.com>
2025-09-20 21:24:25 -0300

    commit 2025 09 20 02
    Implementação de multiplas cargas distribuidas

--- src/PINNViga.py
+++ src/PINNViga.py
@@ -16,7 +16,41 @@ def set_seed(seed=1):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-def physics_loss(model, x):
+def obter_trechos_cargas(lista_cargas, apoio_esq, apoio_dir):
+    posicoes_x = []
+
+    for carga in lista_cargas:
+        if carga.x0 not in posicoes_x:
+            posicoes_x.append(carga.x0)
+        if carga.x1 not in posicoes_x:
+            posicoes_x.append(carga.x1)
+
+    for apoio in [apoio_esq, apoio_dir]:
+        if apoio.x not in posicoes_x:
+            posicoes_x.append(apoio.x)
+
+    posicoes_x = sorted(posicoes_x)
+
+    trechos_cargas = []
+
+    for k in range(len(posicoes_x) -1):
+        dict_trecho = {
+            "x0" : posicoes_x[k],
+            "x1" : posicoes_x[k + 1],
+            "qx" : 0,
+            "qy" : 0
+        }
+
+        for carga in lista_cargas:
+            if carga.x0 <= posicoes_x[k] and carga.x1 >= posicoes_x[k+1]:
+                dict_trecho['qx'] += carga.qx
+                dict_trecho['qy'] += carga.qy
+
+        trechos_cargas.append(dict_trecho)
+
+    return trechos_cargas
+
+def physics_loss(model, x, q):
     u = model(x)
     
     u_x = torch.autograd.grad(u, x, torch.ones_like(u), create_graph=True)[0]
@@ -24,9 +58,57 @@ def physics_loss(model, x):
     u_xxx = torch.autograd.grad(u_xx, x, torch.ones_like(u_xx), create_graph=True)[0]
     u_xxxx = torch.autograd.grad(u_xxx, x, torch.ones_like(u_xxx), create_graph=True)[0]
     
-    f = u_xxxx - 1.0
+    f = u_xxxx - q
 
     return torch.mean((f) ** 2)
+
+def interface_loss(model, x):
+
+    eps = 1e-6
+    x_left = torch.tensor([[x - eps]], requires_grad=True)
+    x_right = torch.tensor([[x + eps]], requires_grad=True)
+
+    u_l = model(x_left)
+    uxl = torch.autograd.grad(u_l, x_left, torch.ones_like(u_l), create_graph=True)[0]
+    uxxl = torch.autograd.grad(uxl, x_left, torch.ones_like(uxl), create_graph=True)[0]
+    uxxxl = torch.autograd.grad(uxxl, x_left, torch.ones_like(uxxl), create_graph=True)[0]
+
+    u_r = model(x_right)
+    uxr = torch.autograd.grad(u_r, x_right, torch.ones_like(u_r), create_graph=True)[0]
+    uxxr = torch.autograd.grad(uxr, x_right, torch.ones_like(uxr), create_graph=True)[0]
+    uxxxr = torch.autograd.grad(uxxr, x_right, torch.ones_like(uxxr), create_graph=True)[0]
+
+    # Continuidade de u, u', u''
+    cont_loss = ((u_l - u_r) ** 2 +
+                (uxl - uxr) ** 2 + 
+                (uxxl - uxxr) ** 2)
+
+    return cont_loss
+
+def trecho_physics_loss(model, trechos_cargas, x):
+
+    loss_pde = 0
+    loss_int = 0
+
+    max_qy = max([abs(carga['qy']) for carga in trechos_cargas])
+
+    for k, carga in enumerate(trechos_cargas):
+        x0 = carga['x0']
+        x1 = carga['x1']
+
+        qx = carga['qx']
+        qy = carga['qy'] / max_qy
+
+        filter = (x >= x0) & (x <= x1)
+        x_train_subdomain = x[filter].view(-1, 1)
+
+        x_train_subdomain.requires_grad_(True)
+        if x_train_subdomain.numel() > 0:
+            loss_pde += physics_loss(model, x_train_subdomain, qy)
+        if x0 != min([carga['x0'] for carga in trechos_cargas]):
+            loss_int += interface_loss(model, x0)
+
+    return loss_pde
 
 def boundary_loss(model, apoio_esq, apoio_dir):
     x0 = torch.tensor([[0.0]], requires_grad=True)
@@ -80,18 +162,17 @@ class PINNViga(nn.Module):
             nn.Linear(64, 1)
         )
 
-        self.log_pde_weight = nn.Parameter(torch.tensor(0.0))
-        self.log_bc_weight = nn.Parameter(torch.tensor(0.0))
-
     def forward(self, x):
         return self.net(x)
+    
+    # def closure():
+    #     optimizer.zero_grad()
 
-    def run_model(self, apoio_esq, apoio_dir, EI, q, L,
+    def run_model(self, apoio_esq, apoio_dir, lista_cargas, EI, L,
                   num_epochs=5000, pde_weight=1.0, bc_weight=1.0,
                   min_delta=1e-6, tol=2e-5, tam=101,
                   patience=200):
         
-        u_ref = q * (L**4) / EI
         set_seed(1)
         L = float(L)
         self.model = PINNViga()
@@ -113,11 +194,26 @@ class PINNViga(nn.Module):
         x_scaled = min_val + (max_val - min_val) * torch.rand(tam, 1)
         x_train = x_scaled.requires_grad_(True)
 
+        self.trechos_cargas = obter_trechos_cargas(lista_cargas, apoio_esq, apoio_dir)
+        max_qy = max_qy = max([abs(carga['qy']) for carga in self.trechos_cargas])
+
+        u_ref = max_qy * (L**4) / EI
+
         for epoch in range(num_epochs + 1):
-            loss_pde = physics_loss(self.model, x_train)
-            loss_bc = boundary_loss(self.model, apoio_esq, apoio_dir)
+            # torch.manual_seed(1)
+            # min_val = 0
+            # max_val = 1
+            # x_scaled = min_val + (max_val - min_val) * torch.rand(tam, 1)
+            # x_train = x_scaled.requires_grad_(True)
+            # loss_pde = physics_loss(self.model, x_train)
+
+            # if epoch >= max([0.3 * num_epochs, 350]):
+            #     optimizer = torch.optim.LBFGS(self.model.parameters(), lr=0.001, max_iter=int(round(0.7 * num_epochs,0)))
 
             optimizer.zero_grad()
+
+            loss_pde = trecho_physics_loss(self.model, self.trechos_cargas, x_train)
+            loss_bc = boundary_loss(self.model, apoio_esq.graus, apoio_dir.graus)
 
             loss = pde_weight * loss_pde + bc_weight * loss_bc
 
@@ -155,22 +251,28 @@ class PINNViga(nn.Module):
             self.loss_pde_variation.append(loss_pde)
             self.loss_bc_variation.append(loss_bc)
 
+            self.x_train = x_train
+
         if best_state is not None:
             self.model.load_state_dict(best_state)
             print(f"Melhor estado restaurado da época {best_epoch} com perda {best_loss:.6e}")
 
         self.loss = self.loss_pde_variation + self.loss_bc_variation
 
-    def plot_errors(self, scale):
+    def plot_errors(self, scale, nome = "erros.png"):
         valores_epoch = np.arange(0, len(self.u_plot_variation), 1)
         valores_bc = [float(loss) for loss in self.loss_bc_variation]
         valores_pde = [float(loss) for loss in self.loss_pde_variation]
         valores_total = [bc + pde for bc, pde in zip(valores_bc, valores_pde)]
-        plt.plot(valores_epoch, valores_bc, label = 'BC Loss', color = 'red', lw = 1)
-        plt.plot(valores_epoch, valores_pde, label = 'PDE Loss', color = 'blue', lw = 1)
-        plt.plot(valores_epoch, valores_total, label = 'Total Loss', color = 'green', lw = 1)
-        plt.xlabel('Epoch')
+
+        plt.figure(figsize=(8, 4.5))
+        plt.plot(valores_epoch, valores_bc, label = 'Perda CC', color = 'red', lw = 1)
+        plt.plot(valores_epoch, valores_pde, label = 'Perda ED', color = 'blue', lw = 1)
+        plt.plot(valores_epoch, valores_total, label = 'Perda Total', color = 'green', lw = 1)
+        plt.title("Variação do erro durante o treinamento")
+        plt.xlabel('Época')
         plt.ylabel('Erro')
         plt.xlim(0, max(valores_epoch))
         plt.ylim(0, 1.05 * max(valores_total) / scale)
         plt.legend()
+        plt.savefig(nome, dpi = 300)
